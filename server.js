63f25b8e6274e24810f1bb1c1ecbbeff6631e84e
kristianreyes7 <kristianreyes7@icloud.com>
2021-08-06 10:48:10 -0700

    Server.js set-up

---
+++ server.js
@@ -0,0 +1,60 @@
+//====Dependencies====//
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const db = mongoose.connection;
+const cors = require('cors')
+const session = require('express-session')
+require ('dotenv').config();
+
+//=====PORT====//
+const PORT = process.env.PORT || 3003;
+
+//====Database====//
+const MONGODB_URI = process.env.MONGODB_URI;
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+//error / success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+//====Middleware====//
+app.use(cors());
+app.use(express.json());
+app.use(
+  session({
+    secret: process.env.SECRET, //a random string do not copy this value or your stuff will get hacked
+    resave: false, // default more info: https://www.npmjs.com/package/express-session#resave
+    saveUninitialized: false // default  more info: https://www.npmjs.com/package/express-sess  ion#resave
+  })
+)
+
+app.get('/', (req, res) => {
+  res.redirect('/workout');
+})
+const isAuthenticated = (req, res, next) => {
+  if (req.session.currentUser) {
+    return next()
+  } else {
+    res.redirect('/sessions/new')
+  }
+}
+
+
+//====Routes====//
+// const recipe_router = require('./routes/recipe_routes.js')
+// app.use('/recipes', isAuthenticated, recipe_router);
+
+// const user_router = require('./routes/users_routes.js')
+// app.use('/users', user_router)
+
+// const sessions_router = require('./routes/sessions_routes.js')
+// app.use('/sessions', sessions_router);
+
+//====listener====//
+app.listen(PORT, () => {
+  console.log('Listening on port:', PORT);
+})
