f0cecee67a16f6d5955eaf4f9f539c87a7d4e995
Doug Moreira <dougmoreira@dougs-mbp.home>
2021-08-09 14:36:02 -0400

    smal changes

--- Controllers/users.js
+++ Controllers/users.js
@@ -42,7 +42,7 @@ try {
           .json({ msg: "No account with this email has been registered." });
   const isMatch = await bcrypt.compare(password, user.password);
   if (!isMatch) return res.status(400).json({ msg: "Invalid credentials." });
-  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+  const token = jwt.sign({ id: user._id }, "" + process.env.JWT_SECRET);
   res.json({
       token,
       user: {
